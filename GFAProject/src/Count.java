e5e04c29684331657820725e7c1b3b8b9276a513
CzimreCs <csaba.czimre@gmail.com>
2023-09-11 12:28:25 +0100

    c6

--- GFAProject/src/Count.java
+++ GFAProject/src/Count.java
@@ -2,7 +2,7 @@ public class Count {
     private int monthOfBirth = 04;
     private int yearOfBirth = 1998;
     public void count(){
-        System.out.println("I born in " + monthOfBirth + " of " + yearOfBirth);
+        System.out.println("Counting the countless" + monthOfBirth + " of " + yearOfBirth);
     }
 
     public int getMonthOfBirt() {
