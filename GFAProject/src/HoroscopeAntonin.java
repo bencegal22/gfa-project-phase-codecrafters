c8298dc9fb33ceca66ae5fd9231a89f73100e81e
Svanto24 <antonin.svoboda@protonmail.com>
2023-09-11 13:14:55 +0200

    created a separate HoroscopeAntonin function as the base for my own branch

---
+++ GFAProject/src/HoroscopeAntonin.java
@@ -0,0 +1,18 @@
+public class HoroscopeAntonin {
+
+    private String horoscope = "Rat";
+    private String month = "december";
+
+    public String getHoroscope() {
+        return horoscope;
+    }
+
+    public void setHoroscope(String horoscope) {
+        this.horoscope = horoscope;
+    }
+
+    public void future (String horoscope, String month) {
+        System.out.println(horoscope);
+        System.out.println(month);
+    }
+}
