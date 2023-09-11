b8af93d108a9d0dc4d654370228d4a826009e05f
CzimreCs <csaba.czimre@gmail.com>
2023-09-11 09:45:51 +0100

    c2

--- GFAProject/src/Cheer.java
+++ GFAProject/src/Cheer.java
@@ -1,5 +1,14 @@
 public class Cheer {
+    private String teamName = "Codecrafters";
     public void cheer(){
-        System.out.println("Codecrafters");
+        System.out.println(teamName);
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
     }
 }
