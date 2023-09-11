ead4965778fee46bb8e3cfdb7ed24f0dc84e3203
Bali83 <balaghe@gmail.com>
2023-09-11 11:20:45 +0200

    d3

--- GFAProject/src/Horoscope.java
+++ GFAProject/src/Horoscope.java
@@ -3,8 +3,17 @@ public class Horoscope {
 
     private String horoscope = "Rat";
 
+    public String getHoroscope() {
+        return horoscope;
+    }
+
+    public void setHoroscope(String horoscope) {
+        this.horoscope = horoscope;
+    }
+
     public void future (String horoscope) {
         System.out.println(horoscope);
     }
+
 
 }
