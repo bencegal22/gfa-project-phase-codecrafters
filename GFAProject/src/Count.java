7af076bedb1c3cf95608602c80710fb4acfe2be8
benceggal22 <bence98417@gmail.com>
2023-09-11 10:48:51 +0200

    b2

--- GFAProject/src/Count.java
+++ GFAProject/src/Count.java
@@ -1,6 +1,6 @@
 public class Count {
-
+    private int age = 25;
     public void count(){
-        System.out.println("I'm 25 years old");
+        System.out.println("I'm " + age + " years old");
     }
 }
